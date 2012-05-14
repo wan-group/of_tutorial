b9c87a0cefb54508a87519a7001a4aaaae7fbbf2
Zhirong Shen <zrshen.sjtu@gmail.com>
2012-05-14 22:29:46 +0800

    update myPtrList

--- Lesson2/myPtrList/myPtrList.C
+++ Lesson2/myPtrList/myPtrList.C
@@ -6,24 +6,24 @@ int main(int argc, char* argv[])
 {
     PtrList<myLib> ptl(3);			 // create a list with three pointers to myLib class
     ptl.set(0,new myLib("shen",101));  // create a pointer to a myLib class as the first element of ptl
-    ptl.set(1,new myLib("shen",102));
-    ptl.set(2,new myLib("shen",103));
+    ptl.set(1,new myLib("ye",102));
+    ptl.set(2,new myLib("liu",103));
 
-    PtrList<myLib>& ptl2=ptl;  // only reference available
+    PtrList<myLib>& ptl2=ptl;  // create a reference to the list
 
-	Info << "ptl " <<endl;
-    forAll(ptl, i)
+	Info << "ptl " <<endl;  
+    forAll(ptl, i)  // output list
     {
         Info << ptl[i] << endl;
     }
 
-	Info << "resize to 2" << endl;
+	Info << "resize to 2" << endl;  // resize the list and remove the last one
 
     ptl.setSize(2);
 
 	Info << "ptl " <<endl;
     Info << ptl << endl;
-	Info << "ptl2" <<endl;
+	Info << "ptl2" <<endl; // output the reference
     Info << ptl2 << endl; // output is the same
 
     return 0;
