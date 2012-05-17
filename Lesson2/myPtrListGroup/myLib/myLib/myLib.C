b1d25bb27579ff7ab94012d439ba4fe259d76dd2
Zhirong Shen <zhirong@zhirong-ThinkPad-T420.(none)>
2012-05-17 22:11:05 +0800

    add example of myPtrListGroup, in which class group is created. class group consists of PtrList of myLib class.

---
+++ Lesson2/myPtrListGroup/myLib/myLib/myLib.C
@@ -0,0 +1,52 @@
+#include "myLib.H"
+
+namespace Foam
+{
+
+myLib::myLib()
+:
+    name_("none"),
+    ID_(0),
+    fullName_("none"),
+	email_("none")
+{
+}
+
+myLib::myLib(word name, label ID, word fullName, word email)
+:
+    name_(name),
+    ID_(ID),
+    fullName_(fullName),
+	email_(email)	
+{} 
+
+myLib::myLib(word name, dictionary dict)
+:
+    name_(name),
+	ID_(readScalar(dict.lookup("ID"))),  // do not forget readScalar
+    fullName_(dict.lookup("fullName")),
+	email_(dict.lookup("email"))
+{}
+
+
+myLib::~myLib()
+{
+    Info << name_ << " is killed " << endl;
+}
+
+
+Ostream& operator<<(Ostream& os, const myLib& ml)
+{
+    os << "name: " <<  ml.name_ << tab  << "ID: " << ml.ID_ 
+	<<	tab << "full name: " << ml.fullName_ 
+	<<	tab << "email: " << ml.email_;
+    return os;
+}
+
+} // namespace Foam
+    
+
+
+
+
+
---
+++ Lesson2/myPtrListGroup/myPtrListGroup.C
@@ -0,0 +1,35 @@
+#include "fvCFD.H"
+#include "myLib.H"
+#include "PtrList.H"
+#include "IOdictionary.H"
+
+int main(int argc, char* argv[])
+{
+
+	// ------
+	//  create PtrList from dictionary
+	// ------
+	
+	#include "setRootCase.H"
+	#include "createTime.H"
+	#include "createMesh.H"
+
+    group g1(mesh);
+
+	Info << nl <<  "Display member name:" << endl;
+
+    g1.displayName();
+
+	Info << nl << "Display member ID:" << endl;
+
+    g1.displayID();
+
+    Info << nl << "Display member email: " << endl;
+
+    g1.displayEmail();
+
+    Info << nl << "Program ends" << nl << endl;
+
+
+    return 0;
+}
