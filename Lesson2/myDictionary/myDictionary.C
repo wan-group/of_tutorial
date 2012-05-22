e586c9b19088347211ac2dc127da2371753449e0
Zhirong Shen <zrshen.sjtu@gmail.com>
2012-05-22 14:47:20 +0800

    update case myDictionary:
    add lookupOrDefault and Switch

--- Lesson2/myDictionary/myDictionary.C
+++ Lesson2/myDictionary/myDictionary.C
@@ -2,6 +2,9 @@
 #include "IFstream.H"
 #include "IOstreams.H"
 #include "vector.H"
+#include "Switch.H"
+#include "IOobject.H"
+
 
 using namespace Foam;
 
@@ -16,11 +19,17 @@ int main()
 	// output table of contents
 	Info << dict.toc() << endl;
 
+    // read Switch
+    Switch sw =  Switch(dict.lookup("readOrNot"));
+
 	// read word
 	word name = dict.lookup("name");  
 
 	// read scalar
 	scalar score = readScalar(dict.lookup("score"));
+
+    // look up with  default option
+    scalar score2 = dict.lookupOrDefault<scalar>("score2",0);   // if found, read. if not, score=0;
 
 	// read vector
 	vector direction = dict.lookup("direction");
@@ -28,14 +37,23 @@ int main()
     // test regular expression
     scalar something = readScalar(dict.lookup("something"));
 
-	Info << "name: " << name << tab << "score:" << score << tab << "direction: " << direction << tab << "something: " << something << endl;
 
+	Info << "readOrNot:" << sw << nl 
+         << "name: " << name << tab << "score:" << score << tab << "score2: " << score2 << nl
+         << "direction: " << direction << tab << "something: " << something << endl;
+
+
+    IOobject::writeDivider(Info);
+    Info << "test subDict" << endl;
 
 	// test subdict
 	dictionary yangDict(dict.subDict("yang"));
 	word yangName =  yangDict.lookup("name");
 	label yangID = readInt(yangDict.lookup("ID"));  // readInt
 	Info << "yang's name: " << yangName << tab << "yang's ID: " << yangID << endl;
+    IOobject::writeDivider(Info);
+
+    Info << nl << "Program ends." << endl;
 
 
 	return 0;
